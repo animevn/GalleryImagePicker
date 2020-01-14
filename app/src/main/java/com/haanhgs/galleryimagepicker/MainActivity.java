9dddf801539c50e4d70caa4c06b1dacb356477e5
animevn <haanhth21@gmail.com>
2020-01-14 10:56:54 +0700

    ok

--- app/src/main/java/com/haanhgs/galleryimagepicker/MainActivity.java
+++ app/src/main/java/com/haanhgs/galleryimagepicker/MainActivity.java
@@ -1,16 +1,89 @@
 package com.haanhgs.galleryimagepicker;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.util.concurrent.ExecutorService;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.ivImage)
+    ImageView ivImage;
+    @BindView(R.id.tvUri)
+    TextView tvUri;
+    @BindView(R.id.tvReal)
+    TextView tvReal;
+    @BindView(R.id.bnChange)
+    Button bnChange;
+    @BindView(R.id.bnShare)
+    Button bnShare;
+
+    private static final int REQUEST = 999;
+    private Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ButterKnife.bind(this);
     }
 
+    private void openGallery(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST);
+    }
 
+    private void loadImageFromGallery(Intent intent){
+        uri = intent.getData();
+        if (uri != null){
+            try{
+                tvUri.setText(uri.toString());
+                tvReal.setText(Repo.getRealPathFromUri(this, uri));
+                Bitmap bitmap = Repo.decodeUri(this, uri, 500);
+                if (bitmap != null)ivImage.setImageBitmap(bitmap);
+            }catch (FileNotFoundException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST &&resultCode == RESULT_OK && data != null){
+            loadImageFromGallery(data);
+        }
+    }
+
+    private void shareImage(){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_STREAM, uri);
+        startActivity(Intent.createChooser(intent, "Choose app to share image"));
+    }
+
+    @OnClick({R.id.bnChange, R.id.bnShare})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.bnChange:
+                openGallery();
+                break;
+            case R.id.bnShare:
+                shareImage();
+                break;
+        }
+    }
 }
---
+++ app/src/main/java/com/haanhgs/galleryimagepicker/Repo.java
@@ -0,0 +1,51 @@
+package com.haanhgs.galleryimagepicker;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class Repo {
+
+    public static Bitmap decodeUri(Context ct, Uri uri, int size)throws FileNotFoundException {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        InputStream inputStream = ct.getContentResolver().openInputStream(uri);
+        BitmapFactory.decodeStream(inputStream, null, options);
+        int tempWidth = options.outWidth;
+        int tempHeight = options.outHeight;
+        int scale = 1;
+        while (tempWidth/2 >= size && tempHeight/2 >= size){
+            tempHeight /= 2;
+            tempWidth /= 2;
+            scale *= 2;
+        }
+        BitmapFactory.Options optionsNew = new BitmapFactory.Options();
+        optionsNew.inSampleSize = scale;
+        InputStream inputStreamNew = ct.getContentResolver().openInputStream(uri);
+        return BitmapFactory.decodeStream(inputStreamNew, null, optionsNew);
+    }
+
+    public static String getRealPathFromUri(Context ct, Uri uri){
+        Cursor cursor = null;
+        try{
+            String[] projection = {MediaStore.Images.Media.DATA};
+            cursor = ct.getContentResolver().query(uri, projection, null, null, null);
+            if (cursor != null){
+                int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                cursor.moveToFirst();
+                return cursor.getString(columnIndex);
+            }else {
+                return null;
+            }
+        }finally {
+            if (cursor != null) cursor.close();
+        }
+    }
+
+}
